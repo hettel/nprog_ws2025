922fbb5195ee0318e65ae8e7094f7c5a19a20a31
Joerg <joerg.hettel@hs-kl.de>
2025-11-14 15:28:47 +0100

    Hashvalue-Berechnung

--- src/main/java/part1/ch14_activity/SequentialSearch.java
+++ src/main/java/part1/ch14_activity/SequentialSearch.java
@@ -1,30 +1,69 @@
 package part1.ch14_activity;
 
-public class SequentialSearch
-{
-   public static void main(String[] args)
-   {
-      System.out.println("Start searching");
-      long startTime = System.currentTimeMillis();
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
-      long counter = 0;
-      String str = "";
-      while (true)
-      {
-         str = Util.getRandomString(100);
-         int hashValue = str.hashCode();
-         counter++;
+public class SequentialSearch {
 
-         if (0 < hashValue && hashValue < 50)
-         {
-            break;
-         }
-      }
-      
-      long endTime = System.currentTimeMillis();
-      System.out.println("Found hash value : " + str.hashCode() );
-      System.out.println("Number of tries  : " + counter);
+	private static class SearchTask implements Callable<String> 
+	{
+		private boolean isRunning = true;
+		
+		@Override
+		public String call() {
 
-      System.out.println("Time elapsed     : " + (endTime - startTime) + " [ms]");
-   }
+			String str = "";
+			while (isRunning) {
+				str = Util.getRandomString(100);
+				int hashValue = str.hashCode();
+				
+				if (0 < hashValue && hashValue < 500) {
+					break;
+				}
+			}
+
+			System.out.println("end " + isRunning );
+			
+			return str;
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		System.out.println("start");
+		
+		int numOfProc = Runtime.getRuntime().availableProcessors();
+		
+		ExecutorService executor = Executors.newFixedThreadPool(numOfProc);
+		CompletionService<String> completionService = new ExecutorCompletionService<>(executor);
+		
+		List<SearchTask> tasks = new ArrayList<>();
+		for(int i=0; i<numOfProc;i++)
+		{
+			SearchTask task = new SearchTask();
+			tasks.add(task);
+			Future<String> future =  completionService.submit(task);
+		}
+		
+		for(int i=0; i<2;i++)
+		{
+			System.out.println( completionService.take().get().hashCode() );
+		}
+		
+		for( SearchTask task: tasks )
+		{
+			task.isRunning = false;
+		}
+		
+		
+		executor.shutdownNow();
+		System.out.println("main done");
+		
+	}
 }
